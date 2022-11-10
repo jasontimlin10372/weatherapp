57fe28b205c7e05bf677a8cc63a86dd131bcb4b2
jasontimlin10372 <106777829+jasontimlin10372@users.noreply.github.com>
2022-11-10 18:46:56 -0500

    Add files via upload

---
+++ weather/assets/css/script.js
@@ -0,0 +1,119 @@
+
+// containers and buttons pulled over
+
+var searchBoxContainerEL = document.getElementById("searchBoxContainer");
+var  cityInputEl = document.getElementById("form-input");
+var searchButton = document.getElementById("btn");
+
+
+//var fullUrl =  "https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=imperial&appid=d12707350df08c4703683ba822b2a53b";
+//console.log(fullUrl);
+
+
+
+var defaultapiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+var myApiKey = "&appid=d12707350df08c4703683ba822b2a53b";
+var farenheit = "&units=imperial"
+
+var formSubmitHandler = function(event) {
+       event.preventDefault();
+      var city = cityInputEl.value.trim()
+var apiUrl = defaultapiUrl + city + farenheit + myApiKey;
+      
+fetch(apiUrl).then(function(response) {
+    console.log(response);
+
+    if(response.ok) {
+        response.json().then(function(data1) {
+            console.log(data1);
+
+    var fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data1.coord.lat}&lon=${data1.coord.lon}&units=imperial&appid=d12707350df08c4703683ba822b2a53b`;
+
+  
+    fetch(fiveDayForecastUrl).then(function(response) {
+        response.json().then(function(data2) {
+            console.log(data2);
+            localStorage.setItem(city,JSON.stringify(data2));
+            var todaysDate = moment().format("L");
+            document.querySelector(".nameAndDate").textContent = "Weather Forecast for " + todaysDate;
+
+            document.querySelector("#temp0").textContent = "Daily Temperature: " + data2.daily[0].temp.day + " ℉";
+            document.querySelector("#wind0").textContent = "Daily wind speed: " + data2.daily[0].wind_speed + " M.P.H";
+            document.querySelector("#humidity0").textContent = "Daily humidity index: " + data2.daily[0].humidity + " %";
+            document.querySelector("#uv0").textContent = "Daily UV index " + data2.daily[0].uvi;
+            //document.querySelector("#icon").textContent = data2.daily[0].weather[0];
+            var day1 = moment().add(1, 'day').format('l');
+            var dateEl1 = document.querySelector("#name1")
+            dateEl1.textContent = day1;
+            document.querySelector("#temp1").textContent = "Temp: " + data2.daily[1].temp.day + " ℉"
+            document.querySelector("#wind1").textContent =  "Wind: " + data2.daily[1].wind_speed + " M.P.H";
+            document.querySelector("#humidity1").textContent = "Humidity: " + data2.daily[1].humidity + " %";
+            //document.querySelector("#temp0").textContent =
+
+            // day 2
+
+            var day2 =  moment().add(2, 'day').format('l');
+            var dateEl2 = document.querySelector("#name2")
+            dateEl2.textContent = day2;
+            document.querySelector("#temp2").textContent = "Temp: " + data2.daily[2].temp.day + " ℉"
+            document.querySelector("#wind2").textContent =  "Wind: " + data2.daily[2].wind_speed + " M.P.H";
+            document.querySelector("#humidity2").textContent = "Humidity: " + data2.daily[2].humidity + " %";
+            //document.querySelector("#temp0").textContent =
+            
+
+            // day 3
+
+            var day3 =  moment().add(3, 'day').format('l');
+            var dateEl3 = document.querySelector("#name3")
+            dateEl3.textContent = day3;
+            document.querySelector("#temp3").textContent = "Temp: " + data2.daily[3].temp.day + " ℉"
+            document.querySelector("#wind3").textContent = "Wind: " + data2.daily[3].wind_speed + " M.P.H";
+            document.querySelector("#humidity3").textContent = "Humidity: " + data2.daily[3].humidity + " %";
+            //document.querySelector("#temp0").textContent =
+            
+
+            // day 4
+
+            var day4 =  moment().add(4, 'day').format('l');
+            var dateEl4 = document.querySelector("#name4")
+            dateEl4.textContent = day4;
+            document.querySelector("#temp4").textContent = "Temp: " + data2.daily[4].temp.day + " ℉"
+            document.querySelector("#wind4").textContent = "Wind: " + data2.daily[4].wind_speed + " M.P.H";
+            document.querySelector("#humidity4").textContent = "Humidity: " + data2.daily[4].humidity + " %";
+            //document.querySelector("#temp0").textContent =
+            
+
+            // day 5
+
+            var day5 =  moment().add(5, 'day').format('l');
+            var dateEl5 = document.querySelector("#name5")
+            dateEl5.textContent = day5;
+            document.querySelector("#temp5").textContent = "Temp: " + data2.daily[5].temp.day + " ℉"
+            document.querySelector("#wind5").textContent = "Wind: " + data2.daily[5].wind_speed + " M.P.H";
+            document.querySelector("#humidity5").textContent = "Humidity: " + data2.daily[5].humidity + " %";
+            //document.querySelector("#temp0").textContent =
+
+          
+        })
+     
+
+    })
+
+
+
+
+
+        })
+    }
+})
+
+
+ }
+
+// Event listener 
+
+
+searchButton.addEventListener("click", formSubmitHandler);
+
+
+    
